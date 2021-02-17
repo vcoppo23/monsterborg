e27613a5c22279b4139ddead4cffc4a9760a3813
vcoppo23 <vcoppo23@bsmschool.org>
2021-02-17 10:53:17 -0600

    MonsterBorg

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo '*** MAKE SURE THAT THE THUNDERBORG SOFTWARE IS INSTALLED ***'
+
+echo '=== Make scripts executable ==='
+chmod a+x *.py
+chmod a+x *.sh
+
+echo '=== Finished ==='
+echo ''
+echo 'Your Raspberry Pi now has the MonsterBorg scripts available'
+echo "They can be found at `pwd`"
---
+++ runMonsterJoy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/home/pi/monsterborg/monsterJoy.py > /dev/null
+
